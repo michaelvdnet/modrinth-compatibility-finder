fcee20d46945fbd2fb3f93651cbd152a96966541
Michael van der Net <michaelvdnet@users.noreply.github.com>
2024-09-17 20:57:01 +0200

    feat: add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,30 @@ import reportWebVitals from "./reportWebVitals";
 
 import "@fontsource/inter";
 import { CssVarsProvider } from "@mui/joy/styles";
+import * as Sentry from '@sentry/react';
+
+
+Sentry.init({
+  dsn: "https://db73627cbb3bda561fe02985beecff23@o136633.ingest.us.sentry.io/4507969259831296",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [],
+  // Set profilesSampleRate to 1.0 to profile every transaction.
+  // Since profilesSampleRate is relative to tracesSampleRate,
+  // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
+  // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
+  // results in 25% of transactions being profiled (0.5*0.5=0.25)
+  profilesSampleRate: 1.0,
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
